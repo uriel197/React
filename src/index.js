de7afb2680e8e9fd66405694607ded12a71d2fe9
uriel197 <60386860+uriel197@users.noreply.github.com>
2024-01-25 12:52:56 +0200

    CSS inLine styles

--- src/index.js
+++ src/index.js
@@ -24,13 +24,17 @@ const Book = () => {
   );
 };
 
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/812XbpMDovL._AC_UL600_SR600,400_.jpg"
-    alt="book cover"
-  />
+const Image = () => <img src="./images/book-1.jpg" alt="book cover" />;
+const Title = () => (
+  <h2 style={{ color: "red", fontSize: "1.2rem" }}>A Court of Mist and Fury</h2>
 );
-const Title = () => <h2>A Court of Mist and Fury</h2>;
+
+// Another way to inline style is:
+// const Title = () => (
+//     const inLineStyling = { color: "red", fontSize: "1.2rem" }
+//   <h2 style={inLineStyling}>A Court of Mist and Fury</h2>
+// );
+
 const Author = () => <h4>by Sarah J. Maas </h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
