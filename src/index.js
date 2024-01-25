c3d9e0080dcb5e46af19dcc0f4090ee6764402e5
uriel197 <60386860+uriel197@users.noreply.github.com>
2024-01-25 10:21:33 +0200

    Components and nested components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// For simple 1 taggers:
+// function Greetings() {
+//   return <h2>My First Component</h2>;
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// another way to do that:
+// function Greetings() {
+//   return React.createElement("h2", {}, "My First Components");
+// }
+
+// to include more than 1 tag:
+// function Greetings() {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h2", {}, "This is our last Component")
+//   );
+// }
+
+// the simpliffied version:
+// function Greeting2() {
+//   return (
+//     <div>
+//       <h2>My Last Simplified Component</h2>
+//     </div>
+//   );
+// }
+
+// nested Components:
+function Greet() {
+  return (
+    <StrictMode>
+      <div>
+        <h2>Uriel Pardes</h2>
+        <Job />
+        <Age />
+      </div>
+    </StrictMode>
+  );
+}
+
+const Job = () => <p>Position: Full Stack Dev</p>;
+const Age = () => <p>Age:47</p>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greet />);
